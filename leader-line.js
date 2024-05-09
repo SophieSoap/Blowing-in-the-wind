8c03d0e4c255ce1e759e9d5aca3f774d5a970878
SophieSoap <sophie.seifert1996@gmail.com>
2024-05-09 23:15:50 +0200

    update leader line Argument 2+3

--- leader-line.js
+++ leader-line.js
@@ -1,10 +1,26 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+  // ARGUMENT 1
   connectElements('argument1-content1', 'argument1-content2');
   connectElements('argument1-content2', 'argument1-content3');
   connectElements('argument1-content3', 'argument1-content4');
   connectElements('argument1-content4', 'argument1-content5');
   connectElements('argument1-content6', 'argument1-content7');
   connectElements('argument1-content7', 'argument1-content8');
+  // ARGUMENT 2
+  connectElements('argument2-content1', 'argument2-content2');
+  connectElements('argument2-content2', 'argument2-content3');
+  connectElements('argument2-content3', 'argument2-content4');
+  connectElements('argument2-content4', 'argument2-content5');
+  connectElements('argument2-content6', 'argument2-content7');
+  connectElements('argument2-content8', 'argument2-content9');
+  connectElements('argument2-content9', 'argument2-content10');
+  connectElements('argument2-content10', 'argument2-content11');
+  // ARGUMENT 3
+  connectElements('argument3-content1', 'argument3-content2');
+  connectElements('argument3-content2', 'argument3-content3');
+  connectElements('argument3-content3', 'argument3-content4');
+  connectElements('argument3-content5', 'argument3-content6');
+  connectElements('argument3-content6', 'argument3-content7');
 });
 
 //function connectElements(className1, className2) {
